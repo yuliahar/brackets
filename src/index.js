55e322237e8a812f276cf51a3d800fe7d3ef045b
Yulia Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2020-03-10 11:36:26 +0300

    feat: add check function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = str.split('');
+    for (let index = 0; index < array.length; index++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let config = bracketsConfig[i];
+            if (config.includes(array[index]))
+                for (let j = 0; j < config.length; j++) {
+                    if (array[index] === config[j] && array[index + 1] === config[j + 1]) {
+                        array.splice(index, 2);
+                        if (index > 2) index -= 2;
+                        else index = -1;
+                        break;
+                    }
+                    else {
+                        i = bracketsConfig.length;
+                        break;
+                    }
+                }
+        }
+    }
+    if (array.length === 0)  return true;
+    else return false;
 }
